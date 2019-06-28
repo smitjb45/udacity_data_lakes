ff620b3bf274032f2f4403f1da4c94d7728f4f5e
smitjb45 <josmith@csumb.edu>
2019-06-28 21:40:08 +0000

    first commit

---
+++ etl.py
@@ -0,0 +1,99 @@
+import configparser
+from datetime import datetime
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, Column
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+
+
+config = configparser.ConfigParser()
+config.read('dl.cfg')
+
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+
+
+def create_spark_session():
+    spark = SparkSession \
+        .builder \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .getOrCreate()
+    return spark
+
+
+def process_song_data(spark, input_data, output_data):
+    # get filepath to song data file
+    song_data = "data/song_data/*/*/*/*.json"
+    
+    # read song data file
+    df = spark.read.json(song_data)
+
+    # extract columns to create songs table
+    global songs_table = df.select(['song_id', 'title', 'artist_id', 'year', 'duration'])
+    
+    # write songs table to parquet files partitioned by year and artist
+    songs_table.write.partitionBy("year", "artist_id").parquet("SongTable.parquet")
+
+    # extract columns to create artists table
+    artists_table = df.select(['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude'])
+    
+    # write artists table to parquet files
+    artists_table.write.parquet("ArtistTable.parquet")
+
+
+def process_log_data(spark, input_data, output_data):
+    # get filepath to log data file
+    log_data = "data/log_data/*.json"
+
+    # read log data file
+    df = spark.read.json("data/log_data/*.json")
+    
+    # filter by NextSong actions for song plays
+    df = df.filter('page = "NextSong"')
+
+    # extract columns for users table    
+    artists_table = df.select(['userId', 'firstName', 'lastName', 'gender', 'level']) 
+    
+    # write users table to parquet files
+    artists_table.write.parquet("UserTable.parquet")
+
+    # create timestamp column from original timestamp column
+    get_timestamp = udf( lambda x: datetime.fromtimestamp(x / 1000).strftime('%Y-%m-%d %H:%M:%S'))
+    df = df.withColumn("time_stamp", get_time_stamp(dfLog.ts))
+    
+    # create datetime column from original timestamp column
+#     get_datetime = udf()
+#     df = 
+    
+    # extract columns to create time table
+    time_table = dfL.select('ts', hour('time_stamp') as 'hour', dayofmonth('time_stamp') as 'day', weekofyear('time_stamp') as 'week' \
+                            , month('time_stamp') as 'month', year('time_stamp') as 'year', date_format('time_stamp', 'EEEE') as 'weekday')
+    
+    # write time table to parquet files partitioned by year and month
+    time_table.write.partitionBy("year", "month").parquet("SongTable.parquet")
+
+    # read in song data to use for songplays table
+    song_df = songs_table
+
+    # extract columns from joined song and log datasets to create songplays table 
+    songplays_table = song_df.join(dfLog).where((artist_and_song.title ==  dfLog.song) & \ 
+                                                (artist_and_song.artist_name == dfLog.artist) & \
+                                                (artist_and_song.duration == dfLog.length)) \
+                                                .select('ts', 'userid', 'level', 'song_id', 'artist_id', \
+                                                        'sessionid', 'location', 'useragent')
+
+    # write songplays table to parquet files partitioned by year and month
+    songplays_table.join(time_table.select('ts', 'year', 'month')).where(songplays_table.ts ==  time_table.ts).write.partitionBy("year", "month").parquet("songplaysTable.parquet")
+
+
+def main():
+    spark = create_spark_session()
+    input_data = "s3a://udacity-dend/"
+    output_data = ""
+    
+    process_song_data(spark, input_data, output_data)    
+    process_log_data(spark, input_data, output_data)
+
+
+if __name__ == "__main__":
+    main()
